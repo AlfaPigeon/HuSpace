ba8573ef15579b31e7c61ba67e2e5cd3660b34ca
AlfaPigeon <51211384+AlfaPigeon@users.noreply.github.com>
2022-12-09 20:46:05 +0300

    new things 2

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+
+    private Rigidbody rb;
+    [Header("Movement")]
+    private Vector3 velocity ;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb =   GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        velocity = new Vector3(speed*Input.GetAxisRaw("Horizontal"), velocity.y, speed* Input.GetAxisRaw("Vertical"));
+
+        rb.velocity = velocity;
+    }
+}
