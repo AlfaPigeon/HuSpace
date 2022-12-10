6bb41cb9171dd7d0c2ae86e3e84d39a3c1ddd13b
draqgamedev <draqgamedev@gmail.com>
2022-12-10 11:17:42 +0300

    Update PlayerScript.cs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -23,25 +23,46 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        velocity = (Input.GetAxisRaw("Horizontal") * movementReference.transform.right + Input.GetAxisRaw("Vertical") * movementReference.forward).normalized * speed;
-        rb.velocity = velocity;
+        UpdatePlayerMovement();
+        UpdateGunRotation();
 
+        //Check for fire
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Fire();
         }
+    }
 
+    private void UpdatePlayerMovement()
+    {
+        velocity = (Input.GetAxisRaw("Horizontal") * movementReference.transform.right + Input.GetAxisRaw("Vertical") * movementReference.forward).normalized * speed;
+        rb.velocity = velocity;
+    }
+
+    private void UpdateGunRotation()
+    {
+        //Get direction between mouse and gun
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(gunPivot.position);
+
+        //Get angle between mouse and gun from direction
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        //Rotate gun by angle
         gunPivot.localRotation = Quaternion.Euler(gunPivot.localRotation.eulerAngles.x, gunPivot.localRotation.eulerAngles.y, angle);
+
+        //Using fake pivot because of bullet movement problem
         fakeGunPivot.transform.localRotation = Quaternion.Euler(90, 0, angle - 30);
     }
 
     public void Fire()
     {
-        //90 - 30 Fake Gun Pivot
+        //Spawn a bullet
         GameObject lastBullet = Instantiate(bullet, barrel.transform.position, Quaternion.identity);
+
+        //Set bullet rotation to gun rotation
         lastBullet.transform.GetChild(1).transform.localRotation = Quaternion.Euler(60f, 30f ,gunPivot.transform.rotation.eulerAngles.z);
+
+        //Move along fake pivot axis
         lastBullet.GetComponent<Rigidbody>().velocity = fakeGunPivot.right * 12.5f;
     }
 }
